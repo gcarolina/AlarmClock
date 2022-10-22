66db7fe08255e85f7442d8368dfb820b8f839c15
gcarolina <karolinaganizoda@gmail.com>
2022-10-23 01:29:41 +0300

    all elements were added

--- AlarmClock/AlarmClock/ViewController.swift
+++ AlarmClock/AlarmClock/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var volumeValue: UIProgressView!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var textFieldVolume: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var timeButton: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var switchClear: UISwitch!
+    @IBOutlet weak var clearButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+    }
+    
+    @IBAction func volumeValueAction(_ sender: UITextField) {
+    }
+    
+    
+    @IBAction func dateAction(_ sender: UIDatePicker) {
+    }
+    
+    
+    @IBAction func clickOnSetTime(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func switchAction(_ sender: UISwitch) {
+    }
+    
+    
+    @IBAction func clickOnClear(_ sender: UIButton) {
+    }
 }
 
